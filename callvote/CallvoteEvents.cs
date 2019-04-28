6464066ed338d97e052703d4053fda48e299edcd
Nicholas Solin <solin.nick@gmail.com>
2019-04-28 02:57:13 -0500

    Make command code a lot more cleaner

--- callvote/CallvoteEvents.cs
+++ callvote/CallvoteEvents.cs
@@ -45,92 +45,47 @@ namespace Callvote
 		public void OnCallCommand(PlayerCallCommandEvent ev)
 		{
 			string command = ev.Command.Split(' ')[0];
-			switch (command)
+
+			int option;
+			if (int.TryParse(command, out option))
 			{
-				case "callvote":
-					string[] quotedArgs = Regex.Matches(string.Join(" ", ev.Command), "[^\\s\"\']+|\"([^\"]*)\"|\'([^\']*)\'")
-						.Cast<Match>()
-						.Select(m => m.Value)
-						.ToArray()
-						.Skip(1)
-						.ToArray();
-					ev.ReturnMessage = this.plugin.startVote(ev.Player, quotedArgs);
-					break;
+				if (this.plugin.currentVote != null)
+				{
 
-				case "stopvote":
-					ev.ReturnMessage = this.plugin.stopVote(ev.Player);
-					break;
+					ev.ReturnMessage = this.plugin.handleVote(ev.Player, option);
+				}
+				else
+				{
+					ev.ReturnMessage = "No vote is in progress.";
+				}
+			}
+			else
+			{
 
-				case "yes":
-				case "no":
-				case "1":
-				case "2":
-				case "3":
-				case "4":
-				case "5":
-				case "6":
-				case "7":
-				case "8":
-				case "9":
-				case "0":
-					if (this.plugin.currentVote != null)
-					{
-						switch (ev.Command)
-						{
-							case "yes":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 1);
-								break;
+				switch (command)
+				{
+					case "callvote":
+						string[] quotedArgs = Regex.Matches(string.Join(" ", ev.Command), "[^\\s\"\']+|\"([^\"]*)\"|\'([^\']*)\'")
+							.Cast<Match>()
+							.Select(m => m.Value)
+							.ToArray()
+							.Skip(1)
+							.ToArray();
+						ev.ReturnMessage = this.plugin.startVote(ev.Player, quotedArgs);
+						break;
 
-							case "no":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 2);
-								break;
+					case "stopvote":
+						ev.ReturnMessage = this.plugin.stopVote(ev.Player);
+						break;
 
-							case "1":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 1);
-								break;
+					case "yes":
+						ev.ReturnMessage = this.plugin.handleVote(ev.Player, 1);
+						break;
 
-							case "2":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 2);
-								break;
-
-							case "3":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 3);
-								break;
-
-							case "4":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 4);
-								break;
-
-							case "5":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 5);
-								break;
-
-							case "6":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 6);
-								break;
-
-							case "7":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 7);
-								break;
-
-							case "8":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 8);
-								break;
-
-							case "9":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 9);
-								break;
-
-							case "0":
-								ev.ReturnMessage = this.plugin.handleVote(ev.Player, 10);
-								break;
-						}
-					}
-					else
-					{
-						ev.ReturnMessage = "No vote is in progress.";
-					}
-					break;
+					case "no":
+						ev.ReturnMessage = this.plugin.handleVote(ev.Player, 2);
+						break;
+				}
 			}
 		}
 
