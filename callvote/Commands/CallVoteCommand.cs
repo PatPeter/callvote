b1c84dd8fb4eebfe09421085e089ba20aaf65118
Nicholas Solin <solin.nick@gmail.com>
2024-10-13 01:24:03 -0500

    Remove unauthorized changes to callvote that violates ethics

--- callvote/Commands/CallVoteCommand.cs
+++ callvote/Commands/CallVoteCommand.cs
@@ -19,7 +19,7 @@ namespace callvote.Commands
 
         public string[] Aliases => null;
 
-        public string Description => "screw pat for putting this off";
+        public string Description => Plugin.Instance.Config.Localizations.CallvoteUsage;
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
