0e277405ab74fd5bdc7cf8ed64cd3d52ed0e832a
Unbistrackted <jvg0dtv@gmail.com>
2024-12-17 15:33:16 -0300

    Updated to EXILED 8 and added new features

---
+++ Commands/ForceResultCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using RemoteAdmin;
+using UnityEngine;
+
+namespace callvote.Commands
+{
+	[CommandHandler(typeof(ClientCommandHandler))]
+	class ForceResultCommand : ICommand
+	{
+		public string Command => "forceresult";
+
+		public string[] Aliases => null;
+
+		public string Description => "";
+
+		public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+		{
+			response = "";
+			Player player = Player.Get(((CommandSender)sender).SenderId);
+			if (sender is PlayerCommandSender)
+			{
+				var plr = sender as PlayerCommandSender;
+				if (player.CheckPermission("cv.superadmin+"))
+				{
+					var args = arguments.Array;
+					if (args.Length > 0)
+					{
+						if (int.TryParse(args[0], out int option))
+						{
+							response = Plugin.Instance.Rigging(option);
+						}
+					}
+				}
+				//response = Plugin.Instance.StopvoteHandler(player);
+			}
+			return false;
+		}
+	}
+}
---
+++ Config.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+using System.Threading.Tasks;
+
+namespace callvote
+{
+
+	public class Config : IConfig
+	{
+		/// <inheritdoc/>
+		[Description("Enable or disable the plugin. Defaults to true.")]
+		public bool IsEnabled { get; set; } = true;
+		public bool Debug { get; set; } = false;
+		public int VoteDuration { get; set; } = 30;
+		public int VoteCooldown { get; set; } = 5;
+		public int MaxAmountOfVotesPerRound { get; set; } = 10;
+		public float MaxWaitGeneral { get; set; } = 0;
+		public float MaxWaitKill { get; set; } = 0;
+		public float MaxWaitKick { get; set; } = 0;
+		public float MaxWaitNuke { get; set; } = 0;
+		public float MaxWaitRespawnWave { get; set; } =  0;
+		public float MaxWaitRestartRound { get; set; } = 0;
+		public float MaxWaitBinary { get; set; } = 0;
+		public float MaxWaitMultipleChoice{ get; set; } = 0;
+		public bool EnableKick { get; set; } = true;
+		public bool EnableKill { get; set; } = true;
+		public bool EnableNuke { get; set; } = true;
+		public bool EnableRespawnWave { get; set; } = true;
+		public bool EnableRestartRound { get; set; } = true;
+
+		public int ThresholdKick { get; set; } = 30;
+		public int ThresholdKill { get; set; } = 30;
+		public int ThresholdNuke { get; set; } = 30;
+		public int ThresholdRespawnWave { get; set; } = 30;
+		public int ThresholdRestartRound { get; set; } = 30;
+	}
+}
