a08451d4e31a237738c4cb8d9a1b6460ffba1d77
Nicholas Solin <solin.nick@gmail.com>
2025-03-03 00:33:38 -0600

    Add NWAPI configs

---
+++ callvote/Configs/Thresholds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callvote.Configs
+{
+	public class Thresholds
+	{
+		/// <inheritdoc/>
+		[Description("Minimum percentage required in order for a Kick vote to succeed.")]
+		public int Kick { get; set; } = 30;
+
+		/// <inheritdoc/>
+		[Description("Minimum percentage required in order for a Kill vote to succeed.")]
+		public int Kill { get; set; } = 30;
+
+		/// <inheritdoc/>
+		[Description("Minimum percentage required in order for a Nuke vote to succeed.")]
+		public int Nuke { get; set; } = 30;
+
+		/// <inheritdoc/>
+		[Description("Minimum percentage required in order for a RespawnWave vote to succeed.")]
+		public int RespawnWave { get; set; } = 30;
+
+		/// <inheritdoc/>
+		[Description("Minimum percentage required in order for a RestartRound vote to succeed.")]
+		public int RestartRound { get; set; } = 30;
+	}
+}
---
+++ callvote/Configs/ToggleCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callvote.Configs
+{
+	public class ToggleCommands
+	{
+		/// <inheritdoc/>
+		[Description("Enable .callvote 'Custom Vote'")]
+		public bool Custom { get; set; } = true;
+
+		/// <inheritdoc/>
+		[Description("Enable .callvote Kick <Player>")]
+		public bool Kick { get; set; } = false;
+
+		/// <inheritdoc/>
+		[Description("Enable .callvote Kill <Player>")]
+		public bool Kill { get; set; } = false;
+
+		/// <inheritdoc/>
+		[Description("Enable .callvote Nuke")]
+		public bool Nuke { get; set; } = false;
+
+		/// <inheritdoc/>
+		[Description("Enable .callvote RespawnWave")]
+		public bool RespawnWave { get; set; } = false;
+
+		/// <inheritdoc/>
+		[Description("Enable .callvote RestartRound")]
+		public bool RestartRound { get; set; } = false;
+	}
+}
